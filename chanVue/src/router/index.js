b303bf79a690c9b329b4b8c8dcc06659356572ef
DESKTOP-HELTQM7\Playdata <shtmdcks06@gmail.com>
2026-01-18 17:49:42 +0900

    [노승찬] - 마스터 이동

--- chanVue/src/router/index.js
+++ chanVue/src/router/index.js
@@ -1,13 +1,17 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Signup from '@/views/user/SignUp.vue'
+import Login from '@/views/user/Login.vue'
+import SignUp from '@/views/user/SignUp.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
-    { path : '/signup', component: () => import ('@/views/user/SignUp.vue') },
-    { path : '/login', component: () => import ('@/views/user/Login.vue') },
+    { path : '/', name: 'home', component},
+    { path : '/signup', component: SignUp },
+    { path : '/login', component: () => import ('@/views/user/Login.vue')},
     { path : '/mypage', component: () => import ('@/views/user/MyPage.vue' ) },
     { path : '/header', component: () => import ('@/components/Header.vue')},
     { path : '/footer', component: () => import ('@/components/Footer.vue')},
